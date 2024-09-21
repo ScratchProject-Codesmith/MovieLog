41d346aeca6a9c54a8c12651c806bf33e8ce18ad
MarkusW <markwaung@gmail.com>
2024-09-21 14:30:07 -0400

    Searchbar-SearchPage-updated

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+//It renders the AppContainer component, which controls the login/signup flow and app content
+import React from 'react';
+import AppContainer from './AppContainer';
+
+const App = () => {
+  return (
+    <div className="app">
+      <AppContainer />
+    </div>
+  );
+};
+
+export default App;
