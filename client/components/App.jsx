087b9e2d61d5cdaedd1903ca6ef976122282510f
pauluhlenkott <pauluhlenkott@gmail.com>
2024-09-21 12:40:48 -0700

    Route search bar to search page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppContainer from '../components/AppContainer.jsx';
+import SearchPage from './SearchPage.jsx';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<AppContainer />} />
+          <Route path='/search' element={<SearchPage />} />
         </Routes>
       </BrowserRouter>
     </div>
