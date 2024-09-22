015c7ddc1aa4272a3e6e53f0916425f9a1505c17
Allie Itczak <allie.itczak@gmail.com>
2024-09-22 17:12:10 -0600

    added middleware to get movie info and get toWatchList

--- server/routes/databaseRouter.js
+++ server/routes/databaseRouter.js
@@ -12,26 +12,40 @@ const router = express.Router();
 //for routes coming to localhost:3000/database/
 
 //route to post database/login
-router.post("/login", databaseController.checkForUser, (req, res) => {
-  return res.status(200).json(res.locals.user);
+router.post("/login", databaseController.verifyUser, (req, res) => {
+  //will be getting request from authslice via credentials on req.body
+  return res.status(200).json({
+    token: res.locals.jwt,
+    user: res.locals.user
+  });
 });
 
 
 //post to database/signup
 router.post("/signup", databaseController.addUser, async (req, res) => {
-  res.status(200).json(res.locals.newUser);
+  //will be getting info from authslice via userinfo on req.body
+  return res.status(200).json(res.locals.newUser);
 });
 
 
 
 //route to post new movie - database/movie
 router.post("/movie", databaseController.addMovie, databaseController.PersonMovie, (req, res) => {
+  //when user clicks correct movie, will add movie to db, and then add to "toWatchList" person movie join talbe
+  //will need personID and movieID
   return res.status(200).json(res.locals.movie);
 });
 
 //route to get movies based on user
+router.post("/movie/list", databaseController.getToWatchList, (req,res)=>{
+  //will need personID
+  return res.status.json(res.locals.toWatchList)
+});
+//
 
-// router.get("/movie");
+router.post('/movie/getInfo', databaseController.getMovieInfo, (req,res)=>{
+  return res.status.json(res.locals.movieInfo)
+})
 
 
 
